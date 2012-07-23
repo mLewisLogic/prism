3d80ea5391127add7df9cf5681549b71b6cb15a3
Mike Lewis <mike@fondu.com>
2012-07-23 15:58:27 -0400

    image rotation fix

--- prism/image_util.py
+++ prism/image_util.py
@@ -4,33 +4,38 @@
 import logging; log = logging.getLogger(__name__)
 
 from cStringIO import StringIO
+import ExifTags
 import hashlib
 import Image
 import ImageFile
 import urllib2
 
-# default is 64k (overcome 'Suspension not allowed here' bug in JPEG decoder)
-ImageFile.MAXBLOCK = 1000000
+# PIL's Error "Suspension not allowed here" work around:
+# s. http://mail.python.org/pipermail/image-sig/1999-August/000816.html
+ImageFile.MAXBLOCK = 1024*1024
 
 
 
 def load_image_from_string(s):
     """Loads a PIL image from a string"""
     try:
-        return Image.open(StringIO(s))
+        image = Image.open(StringIO(s))
+        return ImageHelper(image).fix_rotation()
     except IOError, e:
         log.error(e)
         return None
 
 def load_image_from_file(filename):
     """Loads a PIL image from a file"""
-    return Image.open(filename)
+    image = Image.open(filename)
+    return ImageHelper(image).fix_rotation()
 
 def load_image_from_url(url):
     """Loads a PIL image from a url"""
     try:
         image_string = urllib2.urlopen(url).read()
-        return load_image_from_string(image_string)
+        image = load_image_from_string(image_string)
+        return ImageHelper(image).fix_rotation()
     except urllib2.HTTPError, e:
         log.error(e)
         return None
@@ -73,3 +78,37 @@ class ImageHelper(object):
         out_bytes = StringIO()
         self.image.save(out_bytes, u'PNG', **params)
         return out_bytes.getvalue()
+
+    def fix_rotation(self):
+        """
+        Turns EXIF rotation into a physical rotation (doesn't clear the rotation EXIF!)
+        see: http://stackoverflow.com/questions/1606587/how-to-use-pil-to-resize-and-apply-rotation-exif-information-to-the-file
+        """
+        # Get the key for the 'Orientation' EXIF property
+        for orientation_key in ExifTags.TAGS.iterkeys():
+            if ExifTags.TAGS[orientation_key] is 'Orientation': break
+
+        # Get the EXIF orientation value from our image
+        orientation = self.image._getexif().get(orientation_key)
+
+        # Perform our rotation
+        if orientation is 1: # Horizontal (normal)
+            image = self.image.copy()
+        elif orientation is 2: # Mirrored horizontal
+            image = self.image.transpose(Image.FLIP_LEFT_RIGHT)
+        elif orientation is 3: # Rotated 180
+            image = self.image.transpose(Image.ROTATE_180)
+        elif orientation is 4: # Mirrored vertical
+            image = self.image.transpose(Image.FLIP_TOP_BOTTOM)
+        elif orientation is 5: # Mirrored horizontal then rotated 90 CCW
+            image = self.image.transpose(Image.FLIP_TOP_BOTTOM).transpose(Image.ROTATE_270)
+        elif orientation is 6: # Rotated 90 CW
+            image = self.image.transpose(Image.ROTATE_270)
+        elif orientation is 7: # Mirrored horizontal then rotated 90 CW
+            image = self.image.transpose(Image.FLIP_LEFT_RIGHT).transpose(Image.ROTATE_270)
+        elif orientation is 8: # Rotated 90 CCW
+            image = self.image.transpose(Image.ROTATE_90)
+        else:
+            pass
+
+        return image
